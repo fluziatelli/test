7881fb0742d4feb18a4c54706b3092d4cf30372b
fluziatelli <fluziatelli@gmail.com>
2015-06-24 12:17:32 +0200

    layout opt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,49 @@
+(function(){
+
+'use strict';
+
+yooxTestApp.controller('MainCtrl', function($scope,$location,$routeParams, $anchorScroll) {
+	
+
+	$scope.lookIds = [{
+			"label" : "one",
+			"id" : 1
+		},{
+			"label" : "two",
+			"id" : 2
+		},
+		{
+			"label" : "three",
+			"id" : 3
+		},
+		{
+			"label" : "four",
+			"id" : 4
+	}];
+
+
+	$scope.open = function(item){
+		$location.path('/look/'+item.label);
+		$location.hash('tab');
+	};
+
+	$scope.gotoAnchor = function(id) {
+		var newHash = id;
+			if ($location.hash() !== newHash) {
+		        $location.hash(id);
+		    } else {
+		        $anchorScroll();
+		    }
+	};
+
+	$scope.$watch(function(){
+		return $routeParams.lookId;
+	}, function(current){
+			$scope.tabSelected = current;
+			return true;
+	});
+
+
+});
+
+})();
--- app/scripts/controllers/newsController.js
+++ app/scripts/controllers/newsController.js
@@ -4,7 +4,11 @@
 
 yooxTestApp.controller('NewsCtrl', function($scope,$location,$anchorScroll) {
 
+    $scope.showme=true;
+
 	$scope.gotoAnchor = function(id) {
+		$scope.chiuso= $scope.showme;
+		$scope.showme= !$scope.chiuso;
 		var newHash = id;
 			if ($location.hash() !== newHash) {
 		        $location.hash(id);
